180bf2151d192aed3329c640b0c8bb7830877293
Tanawat Tassana <amp.tanawat@gmail.com>
2022-03-26 10:32:06 +0700

    Disable search button while searching

--- src/search_box/SearchBox.tsx
+++ src/search_box/SearchBox.tsx
@@ -11,11 +11,21 @@ export function SearchBox() {
         value={state.context.searchTerm}
         onChange={handleSearchTermChange}
       ></input>
+      <button
+        onClick={handleSearchClick}
+        disabled={state.value === "searching"}
+      >
+        {state.value === "searching" ? "searching..." : "search"}
+      </button>
     </div>
   );
 
   function handleSearchTermChange(e: ChangeEvent<HTMLInputElement>) {
     const searchTerm = e.target.value;
     send({ type: "search_box__search_term_changed", searchTerm });
+  }
+
+  function handleSearchClick() {
+    send("search_box__search_clicked");
   }
 }
